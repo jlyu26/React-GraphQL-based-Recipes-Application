08c15130100cfeae8a217022b0fdfe9620f2683e
Jinyan.LYU <jlyu@wpi.edu>
2018-08-23 15:08:24 -0400

    Add Apollo-Express and bodyParser middleware, create GraphQL schema.

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({ path: 'variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
+
+// Bring in GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// Create schema
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+});
 
 // Connects to database
 mongoose
@@ -12,6 +26,22 @@ mongoose
 
 // Initializes application
 const app = express();
+
+// Create GraphiQL application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Connect schemas with GraphQL
+app.use(
+	'/graphql', 
+	bodyParser.json(),
+	graphqlExpress({
+		schema,
+		context: {
+			Recipe,
+			User
+		}
+	})
+);
 
 const PORT = process.env.PORT || 4444;
 
