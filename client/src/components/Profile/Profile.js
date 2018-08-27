ea9cdbd18fd3cb187ab2eddde5ba3b7ab5ac83c7
Jinyan.LYU <jlyu@wpi.edu>
2018-08-27 15:49:27 -0400

    Add userRecipes component to profile page, implement getUserRecipes query.

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import UserInfo from './UserInfo';
+import UserRecipes from './UserRecipes';
 
 const Profile = ({ session }) => (
 	<div className='App'>
 		<UserInfo session={session} />
+		<UserRecipes username={session.getCurrentUser.username} />
 	</div>
 );
 
--- schema.js
+++ schema.js
@@ -28,6 +28,7 @@ type Query {
 	searchRecipes(searchTerm: String): [Recipe]
 
 	getCurrentUser: User
+	getUserRecipes(username: String!): [Recipe]
 }
 
 type Token {
